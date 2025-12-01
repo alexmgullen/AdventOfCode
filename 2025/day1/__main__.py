d65d41b871211bdae79b4a3557a3af0bb1354043
alexmgullen <developer@alexmgullen.ca>
2025-12-01 12:59:40 -0500

    2025 day1 part2

--- 2025/day1/__main__.py
+++ 2025/day1/__main__.py
@@ -1,3 +1,5 @@
+import math
+
 def part1(combination):
     total = 0
     current = 50
@@ -19,6 +21,29 @@ def part1(combination):
 
     return total
 
+def part2(combination):
+    total = 0
+    current = 50
+
+    for line in combination.split('\n'):
+        if len(line) > 1:
+
+            ticks =  int(line[1:])
+
+            if line[0] == 'L':
+                for i in range(ticks):
+                    current -= 1
+                    current %= 100
+                    if current == 0:
+                        total += 1
+            elif line[0] == 'R':
+                for i in range(ticks):
+                    current += 1
+                    current %= 100
+                    if current == 0:
+                        total += 1
+    return total
+
 if __name__ == '__main__':
     test_file = open('test1.txt','r')
     test_data = test_file.read()
@@ -32,4 +57,19 @@ if __name__ == '__main__':
     input_file.close()
 
     print(f'part 1 - input: {input_data}')
-    print(f'part 1 - output : {part1(input_data)}')S
+    print(f'part 1 - output : {part1(input_data)}')
+
+    ### Part 2 ###
+
+    print(f'part 2 - test 1 input: {test_data}')
+    print(f'part 2 - test 1 output : {part2(test_data)}')
+
+    test_file = open('test2.txt','r')
+    test_data = test_file.read()
+    test_file.close()
+
+    print(f'part 2 - test 2 input: {test_data}')
+    print(f'part 2 - test 2 output : {part2(test_data)}')
+
+    print(f'part 2 - input: {input_data}')
+    print(f'part 2 - output : {part2(input_data)}')

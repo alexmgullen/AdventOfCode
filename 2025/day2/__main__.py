0b0145fdcf545283b1fedc6308ab8d1d706e69b5
alexmgullen <developer@alexmgullen.ca>
2025-12-02 10:15:41 -0500

    2025 day2 part2

--- 2025/day2/__main__.py
+++ 2025/day2/__main__.py
@@ -1,3 +1,37 @@
+"""
+As you can see from the `is_repeating` function, I also decided to try out some python type annotations and pydoc comments today.
+"""
+
+def part2(ranges):
+    total = 0
+    for r in ranges.split(","):
+        lowest, highest = r.split('-')
+        for i in range(int(lowest), int(highest) + 1):
+            if is_repeating(str(i)):
+                total += i
+
+    return total
+
+def is_repeating(s: str) -> bool:
+    """
+    this function splits each string until the halfway point to determin if the string is made up of reapeating numbers.
+    """
+
+    for i in range(1,(len(s) // 2) + 1):
+        current = s[:i]
+
+        #this effectively divides string 's' into equal parts of size 'i'
+        matches = [s[j:j+i] for j in range(0, len(s), i)]
+
+        matching = True
+        for m in matches[1:]:
+            if current != m:
+                matching = False
+
+        if matching:
+            return True
+
+    return False
 
 def part1(ranges):
     total = 0
@@ -24,3 +58,15 @@ if __name__ == '__main__':
     input_file.close()
 
     print(f'part 1 - output : {part1(input_data)}')
+
+    test_file = open('test1.txt','r')
+    test_data = test_file.read()
+    test_file.close()
+
+    print(f'part 2 - test 1 output : {part2(test_data)}')
+
+    input_file = open('input1.txt','r')
+    input_data = input_file.read()
+    input_file.close()
+
+    print(f'part 2 - output : {part2(input_data)}')
